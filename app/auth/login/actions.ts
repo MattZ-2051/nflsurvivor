618c1d4b2ac3f7fb4c15dd0111fcd10ee80dfae4
Matt Zamora <mzamora2899@gmail.com>
2025-03-29 14:14:12 -0600

    fix: update login signup actions error handling

--- app/auth/login/actions.ts
+++ app/auth/login/actions.ts
@@ -5,42 +5,36 @@ import { redirect } from "next/navigation";
 
 import { createClient } from "@/utils/supabase/server";
 
-export async function login(formData: FormData) {
+export async function loginAction({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
   const supabase = await createClient();
-
   // type-casting here for convenience
   // in practice, you should validate your inputs
   const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
+    email,
+    password,
   };
 
   const { error } = await supabase.auth.signInWithPassword(data);
 
+  if (error && error.message.includes("Invalid login")) {
+    return {
+      error: "No user with that email and password exist",
+    };
+  }
+
   if (error) {
-    redirect("/error");
+    return {
+      error: "Error logging in",
+    };
   }
 
   revalidatePath("/", "layout");
   redirect("/home");
-}
-
-export async function signup(formData: FormData) {
-  const supabase = await createClient();
-
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
-  };
-
-  const { error } = await supabase.auth.signUp(data);
-
-  if (error) {
-    redirect("/error");
-  }
-
-  revalidatePath("/", "layout");
-  redirect("/");
+  return;
 }
